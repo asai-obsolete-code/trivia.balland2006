6f9ab53d7aa83c934e32a7a90d228dbbadd8c14a
Masataro Asai <guicho2.71828@gmail.com>
2015-03-11 01:02:18 +0900

    follow the incompatible change in Trivia optimizer API

--- src/optimizer.lisp
+++ src/optimizer.lisp
@@ -2,49 +2,45 @@
 
 (in-package :trivia.emilie2006)
 
-(defoptimizer :emilie2006 (type clauses)
+(defoptimizer :emilie2006 (clauses)
   (let ((% clauses))
     (iter (for prev = %)
-          (setf % (apply-swapping     % type))
-          (setf % (apply-fusion       % type))
-          (setf % (apply-interleaving % type))
+          (setf % (apply-swapping     %))
+          (setf % (apply-fusion       %))
+          (setf % (apply-interleaving %))
           (until (equal % prev)))
     %))
 
 ;;; Fusion
 
-(defun apply-fusion (clauses &optional (under t))
-  (mapcar #'fuse (divide-clauses under clauses #'fusiblep)))
+(defun apply-fusion (clauses)
+  (mapcar #'fuse (divide-clauses  clauses #'fusiblep)))
 
-(defun divide-clauses (under clauses fn)
+(defun divide-clauses (clauses fn)
   (iter (for c in clauses)
         (with tmp = nil)
         (if (emptyp tmp)
             (push c tmp)
-            (if (apply fn (car tmp) c under)
+            (if (apply fn (car tmp) c)
                 (push c tmp)
                 (progn (collect (nreverse tmp))
                        (setf tmp (list c)))))))
 
-(defun type-equal (t1 t2 &optional (under t))
-  (type= `(and ,t1 ,under)
-         `(and ,t2 ,under)))
-
-(defun type-disjointp (t1 t2 &optional (under t))
-  (subtypep `(and ,under ,t1 ,t2) 'nil))
+(defun type-disjointp (t1 t2)
+  (subtypep `(and ,t1 ,t2) 'nil))
 
 (defun gensym* (name)
   (lambda (x)
     (declare (ignore x))
     (gensym name)))
 
-(defun fusiblep (c1 c2 &optional (under t))
+(defun fusiblep (c1 c2)
   (ematch* (c1 c2)
     (((list ($guard1 s1 (property :type type1) test1 _) _)
       (list ($guard1 s2 (property :type type2) test2 _) _))
      (let ((type1 `(and ,type1 ,(test-type (? s1 test1))))
            (type2 `(and ,type2 ,(test-type (? s2 test2)))))
-       (type-equal type1 type2 under)))))
+       (type= type1 type2)))))
 
 (defun gen-union (&optional x y)
   (union x y :test #'equal))
@@ -76,33 +72,33 @@
                     
 ;;; Interleaving
 
-(defun apply-interleaving (clauses &optional (under t))
+(defun apply-interleaving (clauses)
   ;; be more conservative than Emilie 2006:
   ;; apply only once by each call
   (ematch clauses
     ((list) nil)
     ((list _) clauses)
     ((list* c1 (and rest1 (list* c2 rest2)))
-     (if-let ((c12 (interleave c1 c2 under)))
+     (if-let ((c12 (interleave c1 c2)))
        (cons c12 rest2)
-       (cons c1 (apply-interleaving rest1 under))))))
+       (cons c1 (apply-interleaving rest1))))))
 
-(defun interleave (c1 c2 &optional (under t))
+(defun interleave (c1 c2)
   (ematch* (c1 c2)
     (((list ($guard1 s1 (and o1 (property :type type1)) test1 more1) body1)
       (list ($guard1 s2 (and o2 (property :type type2)) test2 more2) body2))
      (let ((type1 `(and ,type1 ,(test-type (? s1 test1))))
            (type2 `(and ,type2 ,(test-type (? s2 test2)))))
-       (if (type-disjointp type1 type2 under)
+       (if (type-disjointp type1 type2)
            (with-gensyms (il)
-             `((and ,il (type ,under))
+             `((guard ,il t)
                (match* ,il
                  ((guard1 ,(list* s1 o1) ,test1 ,@more1) ,body1)
                  ((guard1 ,(list* s2 o2) t ,@more2) ,body2)))))))))
 
 ;;; Swapping
 
-(defun apply-swapping (clauses &optional (under t))
+(defun apply-swapping (clauses)
   ;; runs swap sort
   (let* ((v (coerce clauses 'vector))
          (len (length v)))
@@ -110,19 +106,19 @@
       (while
           (iter (for i from 1 below len)
                 (for j = (1- i))
-                (when (swappable (aref v i) (aref v j) under)
+                (when (swappable (aref v i) (aref v j))
                   (rotatef (aref v i) (aref v j))
                   (leave t)))))
     (coerce v 'list)))
 
 
-(defun swappable (c1 c2 &optional (under t))
+(defun swappable (c1 c2)
   (ematch* (c1 c2)
     (((list ($guard1 s1 (property :type type1) test1 _) _)
       (list ($guard1 s2 (property :type type2) test2 _) _))
      (let ((type1 `(and ,type1 ,(test-type (? s1 test1))))
            (type2 `(and ,type2 ,(test-type (? s2 test2)))))
-       (and (type-disjointp type1 type2 under)
+       (and (type-disjointp type1 type2)
             (< (sxhash type1) (sxhash type2)))))))
 
 
