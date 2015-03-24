f0dcb713741311a03e25f2eede7ede092320256f
Masataro Asai <guicho2.71828@gmail.com>
2015-03-24 09:41:12 +0900

    variable ordering survey

--- src/column-swapping.lisp
+++ src/column-swapping.lisp
@@ -69,4 +69,68 @@
                 (when (pattern-dependent p1 p2)
                   (in outer (collect (list i j))))))))
 
+;;; Variable Ordering
 
+;; Next, implement the ordering method that satisfy the above
+;; dependency-based ordering.
+
+;; Quote from Rice, Kulhari:
+
+;;     The problem of finding an optimal variable ordering for constructing
+;;     a minimum-size decision diagram (in terms of the number of nodes
+;;     needed to encode the function) is known to be NP-complete [5].
+;;
+;;     The complexity of the variable ordering problem for decision diagram
+;;     construction has forced researchers to establish many efficient
+;;     heuristic and meta-heuristic approaches toward establishing
+;;     near-optimal variable orderings for efficient BDD and
+;;     MDD construction.
+;;
+;;     There are generally considered to be two classes of heuristic
+;;     techniques for establishing efficient variable orderings for
+;;     decision diagram construction: static variable ordering and dynamic
+;;     variable ordering. Static variable ordering techniques attempt to
+;;     establish the variable ordering prior to constructing the actual
+;;     decision diagram, while dynamic variable ordering techiques attempt
+;;     to adjust the ordering online during the actual construction of the
+;;     decision diagram.
+
+;;     Since static heuristics generate the final variable ordering before
+;;     construction of the decision diagram even begins, there is no
+;;     guarantee of good quality solutions from the resulting
+;;     order. Alternatively, since dynamic variable ordering allows for
+;;     adjusting the variable order during the actual construction of the
+;;     decision diagram, they are generally considered more effective in
+;;     pro- viding efficient orderings; however, they are also typically
+;;     much more time consuming in practice than the simpler, static
+;;     heuristics, and thus, are often considered less practical.
+
+;; now in this implementation, we want to implement dynamic ordering of
+;; course :) however, since that paper only describes static ordering, I
+;; implement static ordering methods only.
+
+
+;;; Static Ordering Methods
+
+
+;;;; 2.1 Topological ordering
+;; search from the exit node in breadth-first : work quite well on tree-like circuits.
+;; simple, therefore "baseline technique for comparison"
+;;;; 2.2 Influencial ordering
+;; the most influential of the primary inputs to the function/circuit are
+;; placed earlier on in the ordering.
+;;;;; 2.2.1 Fanin Heuristics
+;;;;; 2.2.2 Subgraph Complexity
+;;;;; 2.2.3 dependent count
+;;;; 2.3 Priority Ordering
+;;;;; 2.3.1 Variable Appending
+;;;;; 2.3.2 Variable Interleaving
+;;;; 2.4 EVAL Meta-Heuristic
+;;;; 2.5 Metric Optimization Heuristics
+;;;;; 2.5.1 Smallest Communication Graph
+;;;;; 2.5.2 Normalized Average Lifetime
+;;;;; 2.5.3 Variable Weighting
+;;;;; 2.5.4 Hybrid Metric Optimization
+;;;;; 2.5.5 MINCE
+;;;;; 2.5.6 FORCE
+;;;;; 2.5.7 Weighted Event Span
