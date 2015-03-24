72f48eebdacfe7c0e93b3a269ab1766efd3b36f0
Masataro Asai <guicho2.71828@gmail.com>
2015-03-24 09:41:04 +0900

    doc

--- src/column-swapping.lisp
+++ src/column-swapping.lisp
@@ -4,42 +4,44 @@
 
 ;; Swapping rule in Balland2006 swaps two clauses on each application. This
 ;; is called a row swapping, in a sense. However, another kind of swapping
-;; can be considered, called column-swapping. This is analogous to the
-;; ordering of variable in Orderd-BDD, which severely affects the quality
-;; of the decision tree.  In our case, it is actually a Multi-valued
-;; decision diagram (MDD).
+;; can be considered, called column-swapping. This is analogous to changing
+;; the ordering of variable in Ordered-BDD, which severely affects the
+;; quality of the decision tree (= size of the decision tree, number of
+;; nodes in a decision tree, or a number of test-forms in a matching
+;; tree). It does not increase the number of checks.
 
 ;; quote from Rice, Kulhari, "A Survey of Static Variable Ordering
 ;; Heuristics for Efficient BDD/MDD Construction" :
 
 ;;     The problem of finding an optimal variable ordering for Binary
 ;;     Decision Diagrams (BDD) or Multi-Valued Decision Diagrams (MDD) is
-;;     widely known to be NP-Complete. This pa- per presents a survey of
-;;     static heuristic techniques applied to ordering the variables of the
-;;     BDD/MDD under construction in order to minimize the overall size of
-;;     the resulting decision diagram.
+;;     widely known to be NP-Complete. ... in order to minimize the overall
+;;     size of the resulting decision diagram.
+
+;;; Dependency Between Patterns
 
 ;; One problem in applying these techniques to pattern matching is the
 ;; dependency between the test forms. For example, A check on a value of a
-;; slot of an object should be done after the object is checked. However,
-;; this is largely no problem since the subpatterns of guard1 are
-;; considered dependent on the pattern.
+;; slot of an object should be done after the parent object is
+;; checked. However, this itself is largely no problem since the
+;; subpatterns of guard1 are considered dependent on the pattern.
 
-;; thus, the remaining degree of freedom is in the ordering of subpatterns,
-;; and the ordering of multipatterns. Since subpatterns are compiled into
+;; The remaining degree of freedom is in the ordering of subpatterns, and
+;; the ordering of multipatterns. Since subpatterns are compiled into
 ;; multipatterns when a fusion occurs, this can simply be reduced to a
 ;; problem of swapping and changing the ordering of multipatterns.
 
-;; However, user-supplied multipatterns are not safe, since they
+;; User-supplied multipatterns are not safe, since they
 ;; may contain references to the variables in the earlier patterns, e.g.,
 ;; 
-;; (match* (a b c)
-;;   (((list a b c) (eq a) (eq b))
-;;    c))
-;; 
-;; now this only applies to the interned, explicit patterns. Therefore, we can check
-;; the variables in (list a b c) appears in (eq a) and (eq b).
-;; Note that this does not require any type information.
+;; (match* (1 2 3) (((list a b c) (eq a) (eq b)) c)) 
+
+;; We check if the variables in (list a b c) appears in (eq a) and (eq b).
+;; Now this only applies to the symbols used in the pattern. Since any
+;; implicitly defined symbols are supposed to be created by gensyms, they
+;; would not overwrap with each other, and thus, we can safely assume that
+;; checking the value returned by `variables' is enough. Note that this
+;; does not require any type information.
 
 (defun pattern-dependent (p1 p2)
   "return true if p2 depends on p1"
